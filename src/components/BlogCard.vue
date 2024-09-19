c4c1d2e00f79b3507d4d41c44ca154f5646e84b2
Elijah SasserCollins <ElijahJSasserCollins@gmail.com>
2024-09-19 14:41:00 -0600

    changes

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,19 +1,18 @@
 <script setup>
 import { Blog } from '@/models/Blog.js';
 
-  const props = defineProps({
-    blogProp: { type: Blog, required: true }
-  })
+const props = defineProps({
+  blogProp: { type: Blog, required: true }
+})
 </script>
 
 
 <template>
   <div class="border border-2 border-dark p-2">
-    
+    <div>Text</div>
+    <div>Img</div>
   </div>
 </template>
 
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
